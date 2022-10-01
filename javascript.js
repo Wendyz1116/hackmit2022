a26d782d789188db2dc6ded61f89ea6f58e8b534
SunKen-Sun <104526059+SunKen-Sun@users.noreply.github.com>
2022-10-01 12:43:36 -0700

    bye

--- javascript.js
+++ javascript.js
@@ -1,16 +1,13 @@
-
-
 /* function openTab(evt, cityName)
 {
   
 }
 */
 
-const startB = document.getElementById('addItem');
-startB.addEventListener('click', add);
+const startB = document.getElementById("addItem");
+startB.addEventListener("click", add);
+console.log("hi");
 
-function add()
-{
-  document.getElementById("foodList").innerHTML = "hi"
+function add() {
+    document.getElementById("foodList").innerHTML = "hi";
 }
-
