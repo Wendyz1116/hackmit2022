c69b6bee599fb9a32a5739c050d330cc0e327711
SunKen-Sun <104526059+SunKen-Sun@users.noreply.github.com>
2022-10-01 14:25:43 -0700

    js1

--- javascript.js
+++ javascript.js
@@ -1,18 +1,31 @@
 /* function openTab(evt, cityName)
 {
   
-}
+}NEED TO GET CSS AND PNG FILES IN THE GIT
 */
 console.log(1);
 const startB = document.getElementById("addItem");
 startB.addEventListener("click", add); //on click it runs add
-/*
+//set("amtUnit", "Enter an Amount");
+var text = "";
+
 function add() {
-    amount = document.querySelector(".amt").value;
-    measurement = document.querySelector(".amtUnit").value;
-    food = documet.querySelector(".item").value;
-    document.getElementById("foodList").innerHTML =
-        amount + " " + measurement + " of " + food;
-    console.log(1);
+    const amount = document.querySelector(".amt").value;
+    const measurement = document.querySelector(".amtUnit").value;
+    const food = document.querySelector(".item").value;
+    if (!amount || !measurement || !food) {
+        exit; //want to also have a text with hidden and remove the hidden to say there was an error
+    } //checks for bad inputs
+    //also would want to try catch to make sure it wont mess with back end
+    text = text + " " + amount + " " + measurement + " of " + food;
+    document.getElementById("foodList").innerHTML = text;
+
+    setText("amt", " ");
+    setText("amtUnit", "Enter a unit");
+    // setText("item", " ");
+    //console.log(amount + " " + measurement);
 }
-*/
+
+function setText(piece, text) {
+    document.querySelector("." + piece).value = text;
+}
