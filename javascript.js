fae66a0de7112051918a5a571052138e53471f32
unknown <wendyzhang1116@gmail.com>
2022-10-01 15:34:48 -0400

    hi

---
+++ javascript.js
@@ -0,0 +1,16 @@
+
+
+/* function openTab(evt, cityName)
+{
+  
+}
+*/
+
+const startB = document.getElementById('addItem');
+startB.addEventListener('click', add);
+
+function add()
+{
+  document.getElementById("foodList").innerHTML = "hi"
+}
+
