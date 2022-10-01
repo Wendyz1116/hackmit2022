00ef67085950589d369d034aef41904c042f0701
SunKen-Sun <104526059+SunKen-Sun@users.noreply.github.com>
2022-10-01 13:10:45 -0700

    t2

--- javascript.js
+++ javascript.js
@@ -3,14 +3,16 @@
   
 }
 */
-
+console.log(1);
 const startB = document.getElementById("addItem");
 startB.addEventListener("click", add); //on click it runs add
-
+/*
 function add() {
     amount = document.querySelector(".amt").value;
     measurement = document.querySelector(".amtUnit").value;
     food = documet.querySelector(".item").value;
     document.getElementById("foodList").innerHTML =
         amount + " " + measurement + " of " + food;
+    console.log(1);
 }
+*/
